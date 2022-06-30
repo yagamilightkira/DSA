e78f10c26e5079033dbfcd315b9de458be441e00
yagamilightkira <codelearner21@gmail.com>
2022-06-30 22:18:43 +0530

    count set bits

---
+++ BitManipulations/countSetBitsinInteger.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countSetBits( int n ) {
+  int count = 0;
+  while( n ) {
+    count += n & 1;
+    n >>= 1;
+  }
+  return count;
+}
+
+int main() {
+  int n = 13;
+  cout << countSetBits(n) << endl;
+  return 0;
+}
