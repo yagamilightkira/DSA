9b9641c1e8d4b26bcf2a9080038363b37b58e014
yagamilightkira <codelearner21@gmail.com>
2022-07-14 01:30:44 +0530

    max and min of arr

---
+++ Array/maxAndMinofArr.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Pair {
+  int min;
+  int max;
+};
+
+Pair maxAndMinArr( int arr[], int n ) {
+  struct Pair minmax;
+  if ( n == 1 ) {
+    minmax.min = arr[0];
+    minmax.max = arr[0];
+  }
+
+  if ( arr[0] > arr[1] ) {
+    minmax.max = arr[0];
+    minmax.min = arr[1];
+  } else {
+    minmax.max = arr[1];
+    minmax.min = arr[0];
+  }
+
+  for ( int i = 2; i < n; i++ ) {
+    if ( arr[i] > minmax.max ) 
+      minmax.max = arr[i];
+    else if ( arr[i] < minmax.min ) 
+      minmax.min = arr[i];
+  }
+
+  return minmax;
+}
+
+int main() {
+ int arr[] = {1, 2, 11, 0, -1 };
+ int n = sizeof(arr)/sizeof(arr[0]);
+
+ struct Pair result = maxAndMinArr( arr, n );
+ cout << " min: " << result.min << " max: " << result.max << endl;
+
+ return 0;
+
+}
